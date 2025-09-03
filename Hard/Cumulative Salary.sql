e63735f704e4b64af48f7fd6670938e1a4382185
aniketjawarkar <aniketnot2026@gmail.com>
2025-09-03 23:01:48 +0530

    Solved

--- Hard/Cumulative Salary.sql
+++ Hard/Cumulative Salary.sql
@@ -42,3 +42,24 @@
 -- | 3  | 3     | 100    |
 -- | 3  | 2     | 40     |
 -- Solution
+
+WITH
+    T AS (
+        SELECT
+            id,
+            month,
+            SUM(salary) OVER (
+                PARTITION BY id
+                ORDER BY month
+                RANGE 2 PRECEDING
+            ) AS salary,
+            RANK() OVER (
+                PARTITION BY id
+                ORDER BY month DESC
+            ) AS rk
+        FROM Employee
+    )
+SELECT id, month, salary
+FROM T
+WHERE rk > 1
+ORDER BY 1, 2 DESC;
